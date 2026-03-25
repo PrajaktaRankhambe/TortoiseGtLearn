bd699df0930eef1e01748cc3559801699996d5f1
PrajaktaRankhambe <prajakta.rankhambe@zensar.com>
2026-03-25 17:39:35 +0530

    Implemented business logic of calculator and Unit Tests for the same

---
+++ New folder/CSharpTutorials/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTutorials
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
---
+++ New folder/CSharpTutorialsTest/CalculatorTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharpTutorials;
+
+namespace CSharpTutorialsTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddNumbers()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void SubstractNumbers()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Sub(30, 3);
+            Assert.AreEqual(27, result);
+
+        }
+        [TestMethod]
+        public void MultipleNumbers()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Mul(30, 3);
+            Assert.AreEqual(90, result);
+
+        }
+        [TestMethod]
+        public void DivisionNumbers()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Div(30, 0);
+            Assert.AreEqual(0, result);
+
+        }
+
+        [TestMethod]
+        public void Calculator_Division_ForDivideByZeroExcpetionTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Div(30, 0));
+        }
+    }
+}
